950b4729b05ad7e8c488f252863463e64160a03a
ShaodongWei <43374853+ShaodongWei@users.noreply.github.com>
2025-06-26 15:56:32 +0200

    Update preprocessing.py

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -5,8 +5,6 @@ from functions import (
     impute_missing,
     log_transform,
     remove_batch_effect,
-    normalize,
-    scale,
     plot_pca
 )
 
@@ -31,8 +29,6 @@ def preprocessing():
     parser.add_argument('--save_intermediate', action='store_true', help='Save intermediate results (default False)')
     parser.add_argument('--disable_plot_PCA', action='store_false', help='Disable PCA plot (default show PCA plot).')
     parser.add_argument('--pseudo_count', type=float, default=1, help='Pseudo count for log transformation (default 1)')
-    parser.add_argument('--normalize', choices=['median', 'quantile'], help='Normalization method')
-    parser.add_argument('--scale', choices=['zscore', 'pareto'], help='Scaling method')
     
     args = parser.parse_args()
 
@@ -52,11 +48,7 @@ def preprocessing():
         raise ValueError(f"Log transformation method '{args.log}' is not supported. Choose 'log2' or 'log10'.")
     if args.impute not in ['pimms', 'knn', 'min']:
         raise ValueError(f"Imputation method '{args.impute}' is not supported. Choose 'pimms', 'knn', or 'min'.")
-    if args.normalize and args.normalize not in ['median', 'quantile']:
-        raise ValueError(f"Normalization method '{args.normalize}' is not supported. Choose 'median' or 'quantile'.")
-    if args.scale and args.scale not in ['zscore', 'pareto']:
-        raise ValueError(f"Scaling method '{args.scale}' is not supported. Choose 'zscore' or 'pareto'.")
-
+    
     # Check if output directory exists, if not create it
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
@@ -82,17 +74,7 @@ def preprocessing():
     if args.disable_plot_PCA:
         plot_pca(data, metadata, batch_col='plate', save_file=os.path.join(args.output_dir, 'pca_batch_correction.pdf'), title='PCA after batch correction')
 
-    ## optional preprocessing steps
-    # Normalize and scale data
-    if args.normalize:
-        data = normalize(data, method=args.normalize)
-
-    # Scale data
-    if args.scale:
-        data = scale(data, method=args.scale)
-
     # Save the cleaned data
-    
     output_file = os.path.join(args.output_dir, 'cleaned_data.tsv')
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
